b396f67d3dc3cd320d44ca53dd2a20373379fd71
EdBozhko <eduard.bozhko92@gmail.com>
2025-04-02 14:35:29 +0300

    feat: add constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const PAGES = {
+  GAME: 'game',
+  CHAT: 'chat',
+  SHOP: 'shop',
+  MORE: 'more',
+
+  STORIES: 'stories',
+  MEDIA: 'media',
+  SETTINGS: 'settings',
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export { getObjectDimensions } from './getObjectDimensions.ts';
+export { PAGES } from './constants.ts';
