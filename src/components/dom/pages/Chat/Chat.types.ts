4f3328838285d355a6082fca7f8f9087bae9734a
EdBozhko <eduard.bozhko92@gmail.com>
2025-04-18 12:01:36 +0300

    fix: Chat types

--- src/components/dom/pages/Chat/Chat.types.ts
+++ src/components/dom/pages/Chat/Chat.types.ts
@@ -1,11 +1,16 @@
 import type { FC } from 'react';
+import type { USER_DATA } from '@src/utils';
 import type { ChoiceOptions } from '@helpers/lib/characterTemplates.types';
 
 export type ChatComponent = FC;
 
 export interface AiMessage {
   message: string;
-  type: string;
+  type: (typeof USER_DATA)[keyof typeof USER_DATA];
   answerOptions?: ChoiceOptions;
 }
 export type AiMessages = AiMessage[];
+
+export interface OptionsInputProps {
+  $checkedColor: string;
+}
